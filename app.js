cbebb9ebcbd3c496d7723f3c8061261ac7c19a81
ykn9080 <ykn9080@empal.com>
2020-03-03 18:35:00 +0900

    Add cors

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
 var express = require('express')
 var graphqlHTTP = require('express-graphql')
 var Graphql = require('graphql')
+
 var fakeDatabase = require('./fakedata.js')
 var app = express();
 var schema = require('./graphql')
+var cors = require('cors');
+app.use(cors())
+
+// app.use(
+//   "/graphql",
+//   bodyParser.json(),
+//   graphqlExpress({
+//     schema,
+//     tracing: true, // 모니터링을 위한 config
+//     cacheControl: true // cache을 위한 config
+//   })
+// );
 
 app.use('/graphql', graphqlHTTP({
     schema: schema,
     graphiql: true,
 }));
+
+// GraphiQL 시각화를 위한 Endpoint
+//app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 app.listen(4000);
 console.log('running a graphql server')
